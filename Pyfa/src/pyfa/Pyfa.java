269e662447f94e56cfd220e379b220bbddeae519
Juan Araujo <juan.araujo@germinare.org.br>
2023-03-21 11:37:25 -0300

    Criada a interface gráfica.

---
+++ Pyfa/src/pyfa/Pyfa.java
@@ -0,0 +1,9 @@
+package pyfa;
+
+public class Pyfa {
+
+    public static void main(String[] args) {
+        new Screen();
+    }
+    
+}
---
+++ Pyfa/src/pyfa/Screen.java
@@ -0,0 +1,13 @@
+package pyfa;
+
+import javax.swing.*;
+
+public class Screen extends JFrame {
+    public Screen(){
+        setVisible(true);
+        setSize(450, 900);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+    }
+}
